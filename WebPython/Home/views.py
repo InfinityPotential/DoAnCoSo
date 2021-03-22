a4ee9e2570ad570562423f77af8c44e280fdfc2d
InfinityPotential <InfinityPotential@.noreply.github.com>
2021-03-22 20:56:26 +0700

    Update

---
+++ WebPython/Home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.https import HttpRespone
+# Create your views here.
+def index(request):
+    respone = HttpRespone()
+    respone.writelines('<h1>Hello world</h1>')
+    respone.write('This is app Home')
+    return respone
