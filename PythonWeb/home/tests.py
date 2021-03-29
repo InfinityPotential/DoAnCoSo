a0252e79e8412a3b6a62ee76cce064de003b0e3d
InfinityPotential <62577792+InfinityPotential@users.noreply.github.com>
2021-03-29 21:33:18 +0700

    Delete End

--- PythonWeb/home/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-# Create your tests here.
-class SimpleTest(SimpleTestCase):
-    def test_home_page_status(self):
-        respone = self.client.get('/')
-        self.assertEquals(respone.status_code, 404)
--- PythonWeb/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    respone = HttpResponse()
-    respone.writelines('<h1>Hello World</h1>')
-    respone.write('This is app home')
-    return respone
