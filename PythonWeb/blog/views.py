f959780a07d863e202a086b1c4433300311aa3bd
InfinityPotential <62577792+InfinityPotential@users.noreply.github.com>
2021-06-11 17:15:20 +0700

    Finale

--- PythonWeb/blog/views.py
+++ PythonWeb/blog/views.py
@@ -5,10 +5,9 @@ from django.http import Http404
 def list(request):
    Data = {'Posts': Post.objects.all().order_by('-date')}
    return render(request, 'blog/blog.html', Data)
-def post(request, id, image):
+def post(request, id):
     try:
         post = Post.objects.get(id=id)
-        post = Post.objects.get(image = image)
     except Post.DoesNotExist:
         raise Http404("Bài viết không tồn tại")
     
